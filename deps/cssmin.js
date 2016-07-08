77d5cb99d6e51f6d61dda3075624c7c73b7fe475
Jeffrey van Norden <jeffreyvannorden@grexx.net>
2016-07-08 12:51:32 +0200

    formatting the code / code style

--- deps/cssmin.js
+++ deps/cssmin.js
@@ -2,77 +2,80 @@
  * cssmin.js
  * Author: Stoyan Stefanov - http://phpied.com/
  * This is a JavaScript port of the CSS minification tool
- * distributed with YUICompressor, itself a port 
- * of the cssmin utility by Isaac Schlueter - http://foohack.com/ 
+ * distributed with YUICompressor, itself a port
+ * of the cssmin utility by Isaac Schlueter - http://foohack.com/
  * Permission is hereby granted to use the JavaScript version under the same
  * conditions as the YUICompressor (original YUICompressor note below).
  */
- 
-/*
-* YUI Compressor
-* Author: Julien Lecomte - http://www.julienlecomte.net/
-* Copyright (c) 2009 Yahoo! Inc. All rights reserved.
-* The copyrights embodied in the content of this file are licensed
-* by Yahoo! Inc. under the BSD (revised) open source license.
-*/
-var YAHOO = YAHOO || {};
-YAHOO.compressor = YAHOO.compressor || {};
-YAHOO.compressor.cssmin = function (css, linebreakpos){
 
-    var startIndex = 0, 
-        endIndex = 0,
-        iemac = false,
-        preserve = false,
-        i = 0, max = 0,
+/*
+ * YUI Compressor
+ * Author: Julien Lecomte - http://www.julienlecomte.net/
+ * Copyright (c) 2009 Yahoo! Inc. All rights reserved.
+ * The copyrights embodied in the content of this file are licensed
+ * by Yahoo! Inc. under the BSD (revised) open source license.
+ */
+var YAHOO               = YAHOO || {};
+YAHOO.compressor        = YAHOO.compressor || {};
+YAHOO.compressor.cssmin = function ( css, linebreakpos ) {
+
+    var startIndex      = 0,
+        endIndex        = 0,
+        iemac           = false,
+        preserve        = false,
+        i               = 0,
+        max,
         preservedTokens = [],
-        token = '';
+        token           = '';
 
     // preserve strings so their content doesn't get accidentally minified
-    css = css.replace(/("([^\\"\n]|\\.|\\)*")|('([^\\'\n]|\\.|\\)*')/g, function(match) {
+    css = css.replace(/("([^\\"\n]|\\.|\\)*")|('([^\\'\n]|\\.|\\)*')/g, function ( match ) {
         var quote = match[0];
         preservedTokens.push(match.slice(1, -1));
         return quote + "___YUICSSMIN_PRESERVED_TOKEN_" + (preservedTokens.length - 1) + "___" + quote;
     });
 
     // Remove all comment blocks...
-    while ((startIndex = css.indexOf("/*", startIndex)) >= 0) {
+    while ( (startIndex = css.indexOf("/*", startIndex)) >= 0 ) {
         preserve = css.length > startIndex + 2 && css[startIndex + 2] === '!';
         endIndex = css.indexOf("*/", startIndex + 2);
-        if (endIndex < 0) {
-            if (!preserve) {
+        if ( endIndex < 0 ) {
+            if ( !preserve ) {
                 css = css.slice(0, startIndex);
             }
-        } else if (endIndex >= startIndex + 2) {
-            if (css[endIndex - 1] === '\\') {
+        } else if ( endIndex >= startIndex + 2 ) {
+            if ( css[endIndex - 1] === '\\' ) {
                 // Looks like a comment to hide rules from IE Mac.
                 // Leave this comment, and the following one, but shorten them
-                css = css.slice(0, startIndex) + "/*\\*/" + css.slice(endIndex + 2);
+                css   = css.slice(0, startIndex) + "/*\\*/" + css.slice(endIndex + 2);
                 startIndex += 5;
                 iemac = true;
-            } else if (iemac && !preserve) {
-                css = css.slice(0, startIndex) + "/**/" + css.slice(endIndex + 2);
+            } else if ( iemac && !preserve ) {
+                css   = css.slice(0, startIndex) + "/**/" + css.slice(endIndex + 2);
                 startIndex += 4;
                 iemac = false;
-            } else if (!preserve) {
+            } else if ( !preserve ) {
                 css = css.slice(0, startIndex) + css.slice(endIndex + 2);
             } else {
                 // preserve
-                token = css.slice(startIndex+3, endIndex); // 3 is "/*!".length
+                token = css.slice(startIndex + 3, endIndex); // 3 is "/*!".length
                 preservedTokens.push(token);
-                css = css.slice(0, startIndex+2) + "___YUICSSMIN_PRESERVED_TOKEN_" + (preservedTokens.length - 1) + "___" + css.slice(endIndex);
-                if (iemac) iemac = false;
+                css = css.slice(0, startIndex + 2) + "___YUICSSMIN_PRESERVED_TOKEN_" + (preservedTokens.length - 1) + "___" + css.slice(endIndex);
+                if ( iemac ) {
+                    iemac = false;
+                }
                 startIndex += 2;
             }
         }
     }
-    
+
     // Normalize all whitespace strings to single spaces. Easier to work with that way.
     css = css.replace(/\s+/g, " ");
 
     // Remove the spaces before the things that should not have spaces before them.
     // But, be careful not to turn "p :link {...}" into "p:link{...}"
     // Swap out any pseudo-class colons with the token, and then swap back.
-    css = css.replace(/(^|\})(([^\{:])+:)+([^\{]*\{)/g, function(m) {
+    css = css.replace(/(^|\})(([^\{:])+:)+([^\{]*\{)/g, function ( m ) {
         return m.replace(":", "___YUICSSMIN_PSEUDOCLASSCOLON___");
     });
     css = css.replace(/\s+([!{};:>+\(\)\],])/g, '$1');
@@ -80,19 +83,17 @@ YAHOO.compressor.cssmin = function (css, linebreakpos){
 
     // retain space for special IE6 cases
     css = css.replace(/:first-(line|letter)({|,)/g, ":first-$1 $2");
-        
+
     // no space after the end of a preserved comment
-    css = css.replace(/\*\/ /g, '*/'); 
-    
-     
+    css = css.replace(/\*\/ /g, '*/');
+
     // If there is a @charset, then only allow one, and push to the top of the file.
     css = css.replace(/^(.*)(@charset "[^"]*";)/gi, '$2$1');
     css = css.replace(/^(\s*@charset [^;]+;\s*)+/gi, '$1');
-    
+
     // Put the space back in some cases, to support stuff like
     // @media screen and (-webkit-min-device-pixel-ratio:0){
     css = css.replace(/\band\(/gi, "and (");
-    
 
     // Remove the spaces after the things that should not have spaces after them.
     css = css.replace(/([!{}:;>+\(\[,])\s+/g, '$1');
@@ -115,17 +116,16 @@ YAHOO.compressor.cssmin = function (css, linebreakpos){
 
     // Shorten colors from rgb(51,102,153) to #336699
     // This makes it more likely that it'll get further compressed in the next step.
-    css = css.replace(/rgb\s*\(\s*([0-9,\s]+)\s*\)/gi, function(){
+    css = css.replace(/rgb\s*\(\s*([0-9,\s]+)\s*\)/gi, function () {
         var rgbcolors = arguments[1].split(',');
-        for (var i = 0; i < rgbcolors.length; i++) {
+        for ( var i = 0; i < rgbcolors.length; i++ ) {
             rgbcolors[i] = parseInt(rgbcolors[i], 10).toString(16);
-            if (rgbcolors[i].length === 1) {
+            if ( rgbcolors[i].length === 1 ) {
                 rgbcolors[i] = '0' + rgbcolors[i];
             }
         }
         return '#' + rgbcolors.join('');
     });
-    
 
     // Shorten colors from #AABBCC to #ABC. Note that we want to make sure
     // the color is not preceded by either ", " or =. Indeed, the property
@@ -133,7 +133,7 @@ YAHOO.compressor.cssmin = function (css, linebreakpos){
     // would become
     //     filter: chroma(color="#FFF");
     // which makes the filter break in IE.
-    css = css.replace(/([^"'=\s])(\s*)#([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])/gi, function(){ 
+    css = css.replace(/([^"'=\s])(\s*)#([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])([0-9a-f])/gi, function () {
         var group = arguments;
         if (
             group[3].toLowerCase() === group[4].toLowerCase() &&
@@ -145,20 +145,19 @@ YAHOO.compressor.cssmin = function (css, linebreakpos){
             return group[0].toLowerCase();
         }
     });
-    
 
     // Remove empty rules.
     css = css.replace(/[^\};\{\/]+\{\}/g, "");
 
-    if (linebreakpos >= 0) {
+    if ( linebreakpos >= 0 ) {
         // Some source control tools don't like it when files containing lines longer
         // than, say 8000 characters, are checked in. The linebreak option is used in
         // that case to split long lines after a specific column.
-        startIndex = 0; 
-        i = 0;
-        while (i < css.length) {
-            if (css[i++] === '}' && i - startIndex > linebreakpos) {
-                css = css.slice(0, i) + '\n' + css.slice(i);
+        startIndex = 0;
+        i          = 0;
+        while ( i < css.length ) {
+            if ( css[i++] === '}' && i - startIndex > linebreakpos ) {
+                css        = css.slice(0, i) + '\n' + css.slice(i);
                 startIndex = i;
             }
         }
@@ -169,14 +168,14 @@ YAHOO.compressor.cssmin = function (css, linebreakpos){
     css = css.replace(/;;+/g, ";");
 
     // restore preserved comments and strings
-    for(i = 0, max = preservedTokens.length; i < max; i++) {
+    for ( i = 0, max = preservedTokens.length; i < max; i++ ) {
         css = css.replace("___YUICSSMIN_PRESERVED_TOKEN_" + i + "___", preservedTokens[i]);
     }
-    
+
     // Trim the final string (for any leading or trailing white spaces)
     css = css.replace(/^\s+|\s+$/g, "");
 
     return css;
 
 };
-exports.minify = YAHOO.compressor.cssmin;
+exports.minify          = YAHOO.compressor.cssmin;
